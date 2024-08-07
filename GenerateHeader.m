d0c0da34c6285528bc419c59138a2b833be67fda
yhchow0204 <167393137+yhchow0204@users.noreply.github.com>
2024-08-07 21:55:29 +0800

    Add files via upload

---
+++ GenerateHeader.m
@@ -0,0 +1,59 @@
+%Preferable to clear all the memory first before starts the script
+clc
+clearvars
+clear
+
+%% Enter the file name  always change from here
+%  main
+Input = 'KNNClassifier_Test';
+
+generateHeader(Input);
+
+%% Local Function starts here
+
+function generateHeader(fileName)
+    % generateHeader generates a header for a MATLAB file
+    % containing the FileName, MATLAB version, Date Created,
+    % and Programmer's Name.
+    %
+    % Arguments:
+    %    fileName (string): The name of the file.
+
+    % Constants
+    programmerName = getenv('USERNAME');
+    matlabVersion = version;
+
+    % Get the current date
+    dateCreated = datestr(now, 'yyyy-mm-dd');
+
+    % Define the header contents
+    headerContents = {
+        ['FileName: ', fileName]
+        ['MATLAB Version: ', matlabVersion]
+        ['Date Created: ', dateCreated]
+        ['Programmer: ', programmerName]
+    };
+
+    % Determine the length of the longest line
+    maxLength = max(cellfun(@length, headerContents));
+
+    % Create the header with maximum space before %
+    header = '';
+    for i = 1:length(headerContents)
+        content = headerContents{i};
+        padding = repmat(' ', 1, maxLength - length(content) + 11); % 11 is the extra padding for maximum space
+        header = sprintf('%s%% %s%s%%\n%% %s %%\n', header, content, padding, repmat(' ', 1, maxLength + 10)); % Add each line and a blank line with %
+    end
+
+    % Add the underline
+    underline = repmat('_', 1, maxLength + 10); % Adjust the underline length to match the content lines
+    header = sprintf('%s%% %s %%\n', header, underline);
+
+    % Print the header
+    disp(header);
+end
+
+
+
+
+
